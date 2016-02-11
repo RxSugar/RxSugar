623a1dad834a018bb347e5e04371617ad03358eb
JARinteractive <jarinteractive@gmail.com>
2016-02-11 13:17:50 -0600

    Update UISwitch to use updated ValueBinding

--- RxSugar/UISwitch+Sugar.swift
+++ RxSugar/UISwitch+Sugar.swift
@@ -7,9 +7,7 @@ public extension Sugar where HostType: UISwitch {
     public var on: ValueBinding<Bool> {
         return controlValueBinding(
             valueChangeEventTypes: .ValueChanged,
-            valueGetter: { $0.on },
-            valueSetter: { control, value in
-                control.on = value
-        })
+            getter: { $0.on },
+            setter: { $0.on = $1 })
     }
 }
