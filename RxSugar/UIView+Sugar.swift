80bf2e726e68effb5b99bb60b5772bb3feba3e9b
dev1 <dev1@pjtv-md2.asynchrony.com>
2016-05-16 15:28:32 -0500

    added layout and focus engine Sugar for UIView

--- RxSugar/UIView+Sugar.swift
+++ RxSugar/UIView+Sugar.swift
@@ -7,4 +7,22 @@ public extension Sugar where HostType: UIView {
      RxSugar trigger for setNeedsLayout.
      */
     public var setNeedsLayout: AnyObserver<Void> { return valueSetter { (host, _) in host.setNeedsLayout() } }
+    
+    /**
+     RxSugar trigger for layoutIfNeeded.
+     */
+    public var layoutIfNeeded: AnyObserver<Void> { return valueSetter { (host, _) in host.layoutIfNeeded() } }
+
+    #if os(tvOS)
+    /**
+     RxSugar trigger for setNeedsFocusUpdate.
+     */
+    public var setNeedsFocusUpdate: AnyObserver<Void> { return valueSetter { (host, _) in host.setNeedsFocusUpdate() } }
+    
+    /**
+     RxSugar trigger for updateFocusIfNeeded.
+     */
+    public var updateFocusIfNeeded: AnyObserver<Void> { return valueSetter { (host, _) in host.updateFocusIfNeeded() } }
+
+    #endif
 }
