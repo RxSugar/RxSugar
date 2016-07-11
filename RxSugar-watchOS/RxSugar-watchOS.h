9d470123dba07ec76721786b45f5081006a9383e
JARinteractive <jarinteractive@gmail.com>
2016-07-11 13:41:14 -0500

    watchOS Target

---
+++ RxSugar-watchOS/RxSugar-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  RxSugar-watchOS.h
+//  RxSugar-watchOS
+//
+//  Created by JARinteractive on 7/11/16.
+//  Copyright © 2016 RxSugar. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for RxSugar-watchOS.
+FOUNDATION_EXPORT double RxSugar_watchOSVersionNumber;
+
+//! Project version string for RxSugar-watchOS.
+FOUNDATION_EXPORT const unsigned char RxSugar_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxSugar_watchOS/PublicHeader.h>
+
+
