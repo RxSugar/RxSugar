b9dbc92889ae70b55e2aa750d00f87ed913405f8
JARinteractive <jarinteractive@gmail.com>
2017-05-18 10:52:51 -0500

    Add sugar for common accessibility properties.

--- RxSugar/UIView+Sugar.swift
+++ RxSugar/UIView+Sugar.swift
@@ -27,7 +27,22 @@ public extension Sugar where HostType: UIView {
      RxSugar observer for hidden.
      */
     public var hidden: AnyObserver<Bool> { return valueSetter { (host, hidden) in host.isHidden = hidden } }
-    
+	
+	/**
+	RxSugar observer for accessibilityLabel.
+	*/
+	public var accessibilityLabel: AnyObserver<String?> { return valueSetter { (host, accessibilityLabel) in host.accessibilityLabel = accessibilityLabel } }
+	
+	/**
+	RxSugar observer for accessibilityHint.
+	*/
+	public var accessibilityHint: AnyObserver<String?> { return valueSetter { (host, accessibilityHint) in host.accessibilityHint = accessibilityHint } }
+	
+	/**
+	RxSugar observer for accessibilityValue.
+	*/
+	public var accessibilityValue: AnyObserver<String?> { return valueSetter { (host, accessibilityValue) in host.accessibilityValue = accessibilityValue } }
+	
     #if os(tvOS)
     /**
      RxSugar trigger for setNeedsFocusUpdate.
