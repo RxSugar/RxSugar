3257f069acfd7f2091ab4d784b81df5881bfaa7e
JARinteractive <jarinteractive@gmail.com>
2017-08-31 11:27:47 -0500

    Add additional property support to UILabel

--- RxSugar/UILabel+Sugar.swift
+++ RxSugar/UILabel+Sugar.swift
@@ -12,4 +12,19 @@ public extension Sugar where HostType: UILabel {
 	RxSugar wrapper for attributedText property.
 	*/
 	public var attributedText: AnyObserver<NSAttributedString> { return valueSetter { $0.attributedText = $1 } }
+	
+	/**
+	RxSugar wrapper for textColor property.
+	*/
+	public var textColor: AnyObserver<UIColor> { return valueSetter { $0.textColor = $1 } }
+	
+	/**
+	RxSugar wrapper for isHighlighted property.
+	*/
+	public var isHighlighted: AnyObserver<Bool> { return valueSetter { $0.isHighlighted = $1 } }
+	
+	/**
+	RxSugar wrapper for isHighlighted property.
+	*/
+	public var isEnabled: AnyObserver<Bool> { return valueSetter { $0.isEnabled = $1 } }
 }
