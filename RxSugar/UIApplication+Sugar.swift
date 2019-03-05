4402911d05a43aed5f612d206c9238b0b497c5d7
JARinteractive <jarinteractive@gmail.com>
2019-03-05 16:47:47 -0600

    add content size did change notification

--- RxSugar/UIApplication+Sugar.swift
+++ RxSugar/UIApplication+Sugar.swift
@@ -10,10 +10,27 @@ public extension Sugar where HostType: RXSObject {
             valueGenerator: {  },
             subscribe: { target, action in
                 NotificationCenter.default.addObserver(target, selector: action, name: UIApplication.significantTimeChangeNotification, object: nil)
-            },
+        },
             unsubscribe: { target, _ in
                 NotificationCenter.default.removeObserver(target, name: UIApplication.significantTimeChangeNotification, object: nil)
-            },
+        },
+            complete: host.rxs.onDeinit
+        )
+        return timeChanges.asObservable()
+    }
+    
+    /**
+     RxSugar wrapper for significantTimeChange
+     */
+    public var contentSizeCategoryDidChange: Observable<Void> {
+        let timeChanges = TargetActionObservable<Void>(
+            valueGenerator: {  },
+            subscribe: { target, action in
+                NotificationCenter.default.addObserver(target, selector: action, name: UIContentSizeCategory.didChangeNotification, object: nil)
+        },
+            unsubscribe: { target, _ in
+                NotificationCenter.default.removeObserver(target, name: UIContentSizeCategory.didChangeNotification, object: nil)
+        },
             complete: host.rxs.onDeinit
         )
         return timeChanges.asObservable()
